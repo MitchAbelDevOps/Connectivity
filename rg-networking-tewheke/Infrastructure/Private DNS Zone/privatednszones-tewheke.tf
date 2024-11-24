775dab6ec260cfc366a70cd9cdb65fce80497c70
Mitch Abel <mitch.abel@adaptiv.nz>
2024-11-25 08:54:54 +1300

    Fixed sub directory path

--- rg-networking-tewheke/Infrastructure/Private DNS Zone/privatednszones-tewheke.tf
+++ rg-networking-tewheke/Infrastructure/Private DNS Zone/privatednszones-tewheke.tf
@@ -11,7 +11,7 @@ New Resources
 ***************************************************/
 // KeyVault DNS zone and VNET link
 module "keyvault_dns_zone" {
-  source                      = "github.com/MitchAbelDevOps/DevOps/TerraformModules//PrivateDNSZones"
+  source                      = "github.com/MitchAbelDevOps/DevOps//TerraformModules/PrivateDNSZones"
   name                        = "privatelink.vaultcore.azure.net"
   resource_group_name         = var.resourceGroupName
   virtual_networks_to_link_id = data.azurerm_virtual_network.vnet_integration.id
@@ -19,7 +19,7 @@ module "keyvault_dns_zone" {
 
 // ServiceBus DNS zone and VNET link
 module "servicebus_dns_zone" {
-  source                      = "github.com/MitchAbelDevOps/DevOps/TerraformModules//PrivateDNSZones"
+  source                      = "github.com/MitchAbelDevOps/DevOps//TerraformModules/PrivateDNSZones"
   name                        = "privatelink.servicebus.windows.net"
   resource_group_name         = var.resourceGroupName
   virtual_networks_to_link_id = data.azurerm_virtual_network.vnet_integration.id
@@ -27,35 +27,35 @@ module "servicebus_dns_zone" {
 
 // APIM DNS zones and VNET links
 module "api_gateway_dns_zone" {
-  source                      = "github.com/MitchAbelDevOps/DevOps/TerraformModules//PrivateDNSZones"
+  source                      = "github.com/MitchAbelDevOps/DevOps//TerraformModules/PrivateDNSZones"
   name                        = "azure-api.net"
   resource_group_name         = var.resourceGroupName
   virtual_networks_to_link_id = data.azurerm_virtual_network.vnet_integration.id
 }
 
 module "old_developer_portal_dns_zone" {
-  source                      = "github.com/MitchAbelDevOps/DevOps/TerraformModules//PrivateDNSZones"
+  source                      = "github.com/MitchAbelDevOps/DevOps//TerraformModules/PrivateDNSZones"
   name                        = "portal.azure-api.net"
   resource_group_name         = var.resourceGroupName
   virtual_networks_to_link_id = data.azurerm_virtual_network.vnet_integration.id
 }
 
 module "new_developer_portal_dns_zone" {
-  source                      = "github.com/MitchAbelDevOps/DevOps/TerraformModules//PrivateDNSZones"
+  source                      = "github.com/MitchAbelDevOps/DevOps//TerraformModules/PrivateDNSZones"
   name                        = "developer.azure-api.net"
   resource_group_name         = var.resourceGroupName
   virtual_networks_to_link_id = data.azurerm_virtual_network.vnet_integration.id
 }
 
 module "mgmt_portal_dns_zone" {
-  source                      = "github.com/MitchAbelDevOps/DevOps/TerraformModules//PrivateDNSZones"
+  source                      = "github.com/MitchAbelDevOps/DevOps//TerraformModules/PrivateDNSZones"
   name                        = "management.azure-api.net"
   resource_group_name         = var.resourceGroupName
   virtual_networks_to_link_id = data.azurerm_virtual_network.vnet_integration.id
 }
 
 module "apim_git_dns_zone" {
-  source                      = "github.com/MitchAbelDevOps/DevOps/TerraformModules//PrivateDNSZones"
+  source                      = "github.com/MitchAbelDevOps/DevOps//TerraformModules/PrivateDNSZones"
   name                        = "scm.azure-api.net"
   resource_group_name         = var.resourceGroupName
   virtual_networks_to_link_id = data.azurerm_virtual_network.vnet_integration.id
