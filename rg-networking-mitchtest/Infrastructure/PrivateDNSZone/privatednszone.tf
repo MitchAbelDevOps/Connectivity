55e45d3d057b65e0d1444268f105ee55e2361e3b
mabel <abemi591@student.otago.ac.nz>
2025-03-09 19:03:38 +1300

    Refresh private dns zones Terraform

--- rg-networking-mitchtest/Infrastructure/PrivateDNSZone/privatednszone.tf
+++ rg-networking-mitchtest/Infrastructure/PrivateDNSZone/privatednszone.tf
@@ -2,7 +2,7 @@
 Existing Resources
 ***************************************************/
 data "azurerm_virtual_network" "vnet_integration" {
-  name                = "vnet-${var.resourceSuffix}-${var.environment}-${var.locationSuffix}-01"
+  name                = "vnet-${var.resourceSuffix}-${var.environmentGroup}-${var.locationSuffix}"
   resource_group_name = local.fullResourceGroupName
 }
 
@@ -11,7 +11,7 @@ New Resources
 ***************************************************/
 // KeyVault DNS zone and VNET link
 module "keyvault_dns_zone" {
-  source                      = "github.com/MitchAbelDevOps/DevOps//TerraformModules/PrivateDNSZones"
+  source                      = "git::https://github.com/MitchAbelDevOps/azure-devops-resources//TerraformModules/PrivateDNSZones"
   name                        = "privatelink.vaultcore.azure.net"
   resource_group_name         = local.fullResourceGroupName
   virtual_networks_to_link_id = data.azurerm_virtual_network.vnet_integration.id
@@ -19,7 +19,7 @@ module "keyvault_dns_zone" {
 
 // ServiceBus DNS zone and VNET link
 module "servicebus_dns_zone" {
-  source                      = "github.com/MitchAbelDevOps/DevOps//TerraformModules/PrivateDNSZones"
+  source                      = "git::https://github.com/MitchAbelDevOps/azure-devops-resources//TerraformModules/PrivateDNSZones"
   name                        = "privatelink.servicebus.windows.net"
   resource_group_name         = local.fullResourceGroupName
   virtual_networks_to_link_id = data.azurerm_virtual_network.vnet_integration.id
@@ -27,7 +27,7 @@ module "servicebus_dns_zone" {
 
 // Logic/Function app DNS zone and VNET link
 module "apps_dns_zone" {
-  source                      = "github.com/MitchAbelDevOps/DevOps//TerraformModules/PrivateDNSZones"
+  source                      = "git::https://github.com/MitchAbelDevOps/azure-devops-resources//TerraformModules/PrivateDNSZones"
   name                        = "privatelink.azurewebsites.net"
   resource_group_name         = local.fullResourceGroupName
   virtual_networks_to_link_id = data.azurerm_virtual_network.vnet_integration.id
@@ -35,29 +35,44 @@ module "apps_dns_zone" {
 
 // Storage services DNS zones and VNET links
 module "storage_files_dns_zone" {
-  source                      = "github.com/MitchAbelDevOps/DevOps//TerraformModules/PrivateDNSZones"
+  source                      = "git::https://github.com/MitchAbelDevOps/azure-devops-resources//TerraformModules/PrivateDNSZones"
   name                        = "privatelink.file.core.windows.net"
   resource_group_name         = local.fullResourceGroupName
   virtual_networks_to_link_id = data.azurerm_virtual_network.vnet_integration.id
 }
-
 module "storage_blob_dns_zone" {
-  source                      = "github.com/MitchAbelDevOps/DevOps//TerraformModules/PrivateDNSZones"
+  source                      = "git::https://github.com/MitchAbelDevOps/azure-devops-resources//TerraformModules/PrivateDNSZones"
   name                        = "privatelink.blob.core.windows.net"
   resource_group_name         = local.fullResourceGroupName
   virtual_networks_to_link_id = data.azurerm_virtual_network.vnet_integration.id
 }
-
 module "storage_table_dns_zone" {
-  source                      = "github.com/MitchAbelDevOps/DevOps//TerraformModules/PrivateDNSZones"
+  source                      = "git::https://github.com/MitchAbelDevOps/azure-devops-resources//TerraformModules/PrivateDNSZones"
   name                        = "privatelink.table.core.windows.net"
   resource_group_name         = local.fullResourceGroupName
   virtual_networks_to_link_id = data.azurerm_virtual_network.vnet_integration.id
 }
-
 module "storage_queue_dns_zone" {
-  source                      = "github.com/MitchAbelDevOps/DevOps//TerraformModules/PrivateDNSZones"
+  source                      = "git::https://github.com/MitchAbelDevOps/azure-devops-resources//TerraformModules/PrivateDNSZones"
   name                        = "privatelink.queue.core.windows.net"
+  resource_group_name         = local.fullResourceGroupName
+  virtual_networks_to_link_id = data.azurerm_virtual_network.vnet_integration.id
+}
+
+// APIM DNS zones and VNET links
+// Default APIM gateway, allows direct APIM access for testing
+# module "apim_gateway_default_dns_zone" {
+#   source                      = "git::https://github.com/MitchAbelDevOps/azure-devops-resources//TerraformModules/PrivateDNSZones"
+#   name                        = "azure-api.net"
+#   resource_group_name         = local.fullResourceGroupName
+#   virtual_networks_to_link_id = data.azurerm_virtual_network.vnet_integration.id
+# }
+
+// Custom APIM domain for *.{env}-api.internal.mitchtest.nz
+module "custom_dns_zone" {
+  for_each                    = toset(local.fullCustomDomains)
+  source                      = "git::https://github.com/MitchAbelDevOps/azure-devops-resources//TerraformModules/PrivateDNSZones"
+  name                        = each.value
   resource_group_name         = local.fullResourceGroupName
   virtual_networks_to_link_id = data.azurerm_virtual_network.vnet_integration.id
 }
--- rg-networking-mitchtest/Infrastructure/PrivateDNSZone/variables.tf
+++ rg-networking-mitchtest/Infrastructure/PrivateDNSZone/variables.tf
@@ -21,6 +21,10 @@ variable "environment" {
   description = "Environment"
 }
 
+variable "environmentGroup" {
+  type = string
+}
+
 /**************************************************
 Existing Resource Variables
 ***************************************************/
@@ -34,6 +38,18 @@ variable "resourceGroupName" {
   default     = "rg-networking"
 }
 
+variable "customDomainRoot" {
+  type        = string
+  description = "Custom domain root to apply to Mitchtest services"
+  default     = "internal.mitchtest.nz"
+}
+
 locals {
-  fullResourceGroupName = "${var.resourceGroupName}-${var.resourceSuffix}-${var.environment}-${var.locationSuffix}"
+  fullResourceGroupName = "${var.resourceGroupName}-${var.resourceSuffix}-${var.environmentGroup}-${var.locationSuffix}"
+
+  environment_mapping = var.environmentGroup == "npe" ? ["dev", "test"] : var.environmentGroup == "ppe" ? ["stg", "pwe"] : var.environmentGroup == "prd" ? ["prd"] : []
+  fullCustomDomain = var.environment == "prd" ? "api.${var.customDomainRoot}" : "${var.environment}-api.${var.customDomainRoot}"
+  fullCustomDomains = [for env in local.environment_mapping :
+    env == "prd" ? "api.${var.customDomainRoot}" : "${env}-api.${var.customDomainRoot}"
+  ]
 }
